5bc0e89cea16797795fbe247aa977d5650d9d82b
Cecilie <cecilie@brandt.net>
2021-02-18 10:26:09 +0100

    initial commit

---
+++ src/dk/kea/feb11/Main.java
@@ -0,0 +1,9 @@
+package dk.kea.feb11;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // https://www.w3schools.com/java/java_methods_param.asp
+        // https://ladvien.com/data-analytics-mysql-localhost-setup/
+    }
+}
